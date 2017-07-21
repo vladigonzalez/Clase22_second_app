8d340ec5eb7b0f192b2a88366253909aa0993702
Vladimir <Vladigonzalez@gmail.com>
2017-07-20 21:54:30 -0400

    cuarto commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720234220) do
+ActiveRecord::Schema.define(version: 20170721014222) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
