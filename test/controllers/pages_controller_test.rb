a46fa5e5c15f02289c1762f7093f4957bd724b6b
Vladimir <Vladigonzalez@gmail.com>
2017-07-21 23:18:51 -0400

    quinto commit

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get pages_index_url
+  test "should get landing" do
+    get pages_landing_url
     assert_response :success
   end
 
